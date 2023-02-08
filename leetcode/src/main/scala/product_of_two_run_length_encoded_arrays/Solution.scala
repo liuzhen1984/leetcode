a2ae346dc1a79f8bce81a559dd82d25ab30357b6
liuzhen1984 <liuzhen1984@gmail.com>
2023-02-08 11:06:32 -0800

    implement product of two run length encoded arrays, but there is a memory limit issue

--- leetcode/src/main/scala/product_of_two_run_length_encoded_arrays/Solution.scala
+++ leetcode/src/main/scala/product_of_two_run_length_encoded_arrays/Solution.scala
@@ -5,44 +5,57 @@ import scala.collection.mutable
 object Solution extends App {
   val list1: Array[Array[Int]] = Array(Array(1, 2), Array(4, 5), Array(1, 2), Array(6, 7), Array(6, 7))
 
-  val list2: Array[Array[Int]] = Array(Array(2, 1), Array(4, 1), Array(1, 2), Array(6, 7))
+  val list2: Array[Array[Int]] = Array(Array(2, 1), Array(4, 1), Array(1, 2), Array(6, 7),Array(1,1))
 
   println(findRLEArray(list1, list2))
   println(findRLEArray2(list1, list2))
+  println(productRLA(List((1, 2), (4, 5), (1, 2), (6, 7), (6, 7)), List((2, 1), (4, 1), (1, 2), (6, 7))))
+  println(multiply(list1, list2))
 
   def findRLEArray(encoded1: Array[Array[Int]], encoded2: Array[Array[Int]]): List[List[Int]] = {
-    var decode2 = mutable.ListBuffer[Int]()
-    for (en <- encoded2) {
-      decode2.appendAll(((en(0).toString + ",") * en(1)).split(",").map(_.toInt).toList)
-    }
+    var e1 = 0
+    var e2 = 0
     val resultList = mutable.ListBuffer[List[Int]]()
-    for (en <- encoded1) {
-
-      val tmp = decode2.splitAt(en(1))
-      decode2 = tmp._2
-      val list = (((en(0).toString + ",") * en(1)).split(",").map(_.toInt).toList zip tmp._1).map(i => i._1 * i._2)
-      var tmpList = List[Int]()
-      if(resultList.size>0){
-        tmpList = resultList(resultList.length-1)
-        resultList.remove(resultList.length-1)
+    while (e1 < encoded1.length) {
+      val len = Math.min(encoded1(e1)(1), encoded2(e2)(1))
+      val mut = encoded1(e1)(0) * encoded2(e2)(0)
+      if (resultList.length > 0 && resultList(resultList.length - 1)(0) == mut) {
+        resultList(resultList.length - 1) = List(mut, resultList(resultList.length - 1)(1) + len)
+      } else {
+        resultList.append(List(mut, len))
       }
-
-      for (r <- list) {
-        if (tmpList.size>0 && tmpList(0) == r) {
-          tmpList = List[Int](r, tmpList(1) + 1)
-        } else {
-          if (tmpList.size > 0) {
-            resultList.append(tmpList)
-          }
-          tmpList = List[Int](r, 1)
-        }
-      }
-      if (tmpList.size > 0) {
-        resultList.append(tmpList)
-      }
+      encoded1(e1)(1) -= len
+      encoded2(e2)(1) -= len
+      if (encoded1(e1)(1) == 0) e1 += 1
+      if (encoded2(e2)(1) == 0) e2 += 1
     }
-    resultList.toList
+
+    return resultList.toList
+
   }
+  def multiply(encoded1: Array[Array[Int]], encoded2: Array[Array[Int]]): List[List[Int]] = {
+    val nums1 = encoded1.flatMap(x => Array.fill(x(1))(x(0)))
+    val nums2 = encoded2.flatMap(x => Array.fill(x(1))(x(0)))
+    val prodNums = nums1.zip(nums2).map(x => x._1 * x._2)
+
+    var res = List[List[Int]]()
+    var i = 0
+    while (i < prodNums.length) {
+      var j = i
+      while (j < prodNums.length && prodNums(j) == prodNums(i)) {
+        j += 1
+      }
+      res = List(prodNums(i), j - i) :: res
+      i = j
+    }
+    res.reverse
+  }
+
+  def productRLA(a: List[(Int, Int)], b: List[(Int, Int)]): List[(Int, Int)] = {
+    (a, b).zipped.map { case ((va, la), (vb, lb)) => (va * vb, math.min(la, lb)) }
+  }
+
+
 
   def findRLEArray2(encoded1: Array[Array[Int]], encoded2: Array[Array[Int]]): List[List[Int]] = {
 
@@ -57,7 +70,7 @@ object Solution extends App {
     val resultList = mutable.ListBuffer[List[Int]]()
     var tmpValue = 0
     var tmpList = List[Int]()
-    println((decode1 zip decode2).map(i => i._1 * i._2))
+    //    println((decode1 zip decode2).map(i => i._1 * i._2))
     (decode1 zip decode2).map(i => i._1 * i._2).foreach(r => {
       if (tmpValue == r) {
         tmpList = List[Int](r, tmpList(1) + 1)
