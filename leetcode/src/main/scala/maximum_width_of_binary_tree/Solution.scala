204a2e27c9e12c57e2a2356075bc914f79197560
liuzhen1984 <liuzhen1984@gmail.com>
2023-02-15 10:14:59 -0800

    optimized memory issue

--- leetcode/src/main/scala/maximum_width_of_binary_tree/Solution.scala
+++ leetcode/src/main/scala/maximum_width_of_binary_tree/Solution.scala
@@ -15,45 +15,39 @@ object Solution {
     if (root != null) {
       max = 1
     }
-    def deep(nodeList: List[TreeNode]): Unit = {
-      val list = mutable.ListBuffer[TreeNode]()
-
-      for (node <- nodeList) {
-        if (node != null) {
-          list.append(node.left, node.right)
-        } else {
-          list.append(null, null)
-        }
-        if (list(0) == null) {
-          list.remove(0)
-          if (list(0) == null) {
-            list.remove(0)
-          }
-        }
-      }
-
-      var c = true
-      while (c) {
-        c = false
-        if (list.length == 0) {
-          return
-        }
-        if (list(list.length - 1) == null) {
-          list.remove(list.length - 1)
-          c = true
-        }
-      }
-
-      if (max < list.length) {
-        max = list.length
-      }
-      if (list.length > 0) {
-        deep(list.toList)
+    var result = deep(List(root))
+    if (max < result.length) {
+      max = result.length
+    }
+    while (result.length > 0) {
+      result = deep(result)
+      if (max < result.length) {
+        max = result.length
       }
     }
-
-    deep(List(root))
     max
+  }
+
+  def deep(nodeList: List[TreeNode]): List[TreeNode] = {
+    val list = mutable.ListBuffer[TreeNode]()
+    for (node <- nodeList) {
+      if (node != null) {
+        list.append(node.left, node.right)
+      } else {
+        list.append(null, null)
+      }
+      if (list(0) == null) {
+        list.remove(0)
+        if (list(0) == null) {
+          list.remove(0)
+        }
+      }
+    }
+    while (list.length > 0 && list(list.length - 1) == null) {
+      list.remove(list.length - 1)
+    }
+
+    list.toList
   }
 
 }
